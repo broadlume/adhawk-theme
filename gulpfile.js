27dc15f51c204fca2abc0c2b0477bc7bf73e928e
Ian Ker-Seymer <i.kerseymer@gmail.com>
2016-10-05 16:27:51 -0400

    Clean up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,58 @@
-const gulp        = require('gulp');
+const gulp      = require('gulp');
 const browserSync = require('browser-sync').create();
-const sass        = require('gulp-sass');
-const ghPages     = require('gulp-gh-pages');
-
+const sass      = require('gulp-sass');
+const ghPages   = require('gulp-gh-pages');
+const openUrl   = require('openurl');
 
 gulp.task('html', () => {
-    return gulp.src('./index.html').pipe(gulp.dest('./dist'));
+  return gulp.src('./index.html').pipe(gulp.dest('./dist'));
 });
 
 gulp.task('sass', () => {
-    return gulp.src(['./scss/theme.scss', './custom/custom.scss'])
-        .pipe(sass({
-            includePaths: ['./node_modules/bootstrap-sass/assets/stylesheets'],
-        }))
-        .pipe(gulp.dest('./dist/css'))
-        .pipe(browserSync.stream());
+  return gulp.src(['./scss/theme.scss', './custom/custom.scss'])
+    .pipe(sass({
+      includePaths: ['./node_modules/bootstrap-sass/assets/stylesheets'],
+    }))
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('fonts', () => {
-    return gulp.src('./node_modules/bootstap-sass/assets/fonts/**/*')
-        .pipe(gulp.dest('./dist/fonts'));
+  return gulp.src('./node_modules/bootstap-sass/assets/fonts/**/*')
+    .pipe(gulp.dest('./dist/fonts'));
 });
 
 gulp.task('js', () => {
-    return gulp.src([
-        './node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js',
-        './node_modules/jquery/dist/jquery.min.js',
-        './custom/*.js',
-    ])
-        .pipe(gulp.dest('./dist/js'));
+  return gulp.src([
+    './node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js',
+    './node_modules/jquery/dist/jquery.min.js',
+    './custom/*.js',
+  ])
+    .pipe(gulp.dest('./dist/js'));
 });
 
 gulp.task('serve', ['html', 'sass', 'fonts', 'js'], () => {
-    browserSync.init({
-        open: false,
-        port: 3030,
-        host: 'http://local.tryadhawk.com',
-        server: './dist',
-    });
+  browserSync.init({
+    open: false,
+    port: 3030,
+    host: 'http://local.tryadhawk.com',
+    server: './dist',
+  });
 
-    gulp.watch('scss/*.scss', ['sass']);
-    gulp.watch('index.html').on('change', browserSync.reload);
+  gulp.watch('scss/*.scss', ['sass']);
+  gulp.watch('index.html').on('change', browserSync.reload);
 });
 
-gulp.task('deploy', () => {
-  return gulp.src('./dist/**/*')
-    .pipe(ghPages());
+gulp.task('ghpages', () => {
+  return gulp.src('./dist/**/*').pipe(ghPages());
+});
+
+gulp.task('deploy', ['ghpages'], () => {
+  return openUrl.open('http://adhawk.github.io/adhawk-theme');
 });
 
 gulp.task('watch', ['serve'], () => {
-  return require("openurl").open("http://local.tryadhawk.com:3030");
+  return openUrl.open('http://local.tryadhawk.com:3030');
 });
 
 gulp.task('default', ['watch']);
